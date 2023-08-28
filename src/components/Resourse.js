3ad9cf8e54756bf08abd1a3ab0cd7722df11c712
Sowmiya04Balachandran <sowmiya2641@gmail.com>
2023-08-28 17:28:25 +0530

    product displayed

--- src/components/Resourse.js
+++ src/components/Resourse.js
@@ -1,66 +1,43 @@
 
+import React from 'react';
+
 const productsArr = [
-
     {
-    
-    title: 'Colors',
-    
-    price: 100,
-    
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
-    
+      title: 'Colors',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
     },
-    
     {
-    
-    title: 'Black and white Colors',
-    
-    price: 50,
-    
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
-    
+      title: 'Black and white Colors',
+      price: 50,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
     },
-    
     {
-    
-    title: 'Yellow and Black Colors',
-    
-    price: 70,
-    
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
-    
+      title: 'Yellow and Black Colors',
+      price: 70,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
     },
-    
     {
-    
-    title: 'Blue Color',
-    
-    price: 100,
-    
-    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
-    
+      title: 'Blue Color',
+      price: 100,
+      imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
     }
-    
-    ]
-    
-    
+  ];
 
+const Resource = () => {
+  const elements = (
+    <ul>
+      {productsArr.map((product) => (
+        <li key={product.title}>
+          <h2>{product.title}</h2>
+          <p>Price: ${product.price}</p>
+          <img src={product.imageUrl} alt={product.title} />
+        </li>
+      ))}
+    </ul>
+  );
 
+  return <div>{elements}</div>;
+};
 
-
-const Resourse=()=>{
-
-    const elements=<ul>{productsArr.map((ele)=>
-     title=ele.title,
-     price=ele.price,
-      imageUrl=ele.imageUrl
-    
-      )
-    }</ul>
-
-    return (
-        <div>{elements}</div>
-    )
-
-}
-export default Resourse;
+export default Resource;
