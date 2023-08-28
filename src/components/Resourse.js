3e5c185918c34392fc61e8f1274d5ce19cb34eb7
Sowmiya04Balachandran <sowmiya2641@gmail.com>
2023-08-28 17:09:51 +0530

    first

---
+++ src/components/Resourse.js
@@ -0,0 +1,66 @@
+
+const productsArr = [
+
+    {
+    
+    title: 'Colors',
+    
+    price: 100,
+    
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+    
+    },
+    
+    {
+    
+    title: 'Black and white Colors',
+    
+    price: 50,
+    
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+    
+    },
+    
+    {
+    
+    title: 'Yellow and Black Colors',
+    
+    price: 70,
+    
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+    
+    },
+    
+    {
+    
+    title: 'Blue Color',
+    
+    price: 100,
+    
+    imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+    
+    }
+    
+    ]
+    
+    
+
+
+
+
+const Resourse=()=>{
+
+    const elements=<ul>{productsArr.map((ele)=>
+     title=ele.title,
+     price=ele.price,
+      imageUrl=ele.imageUrl
+    
+      )
+    }</ul>
+
+    return (
+        <div>{elements}</div>
+    )
+
+}
+export default Resourse;
